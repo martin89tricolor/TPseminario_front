be2f90f1cdc5c00bf278a2b25926cb64b2d5ab0e
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-11-14 21:21:12 -0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,14 @@ import { Helmet } from 'react-helmet';
 import {
   Box,
   Container,
+  Grid,
+  ImageListItem,
+  ImageList,
   Card,
 } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';  
+import Typography from '@material-ui/core/Typography';
+import AboutPhoto from 'src/components/about/AboutPhoto'  
+import AboutPhoto2 from 'src/components/about/AboutPhoto2' 
 
 const About = () => (
   <>
@@ -21,9 +26,36 @@ const About = () => (
     <Card>
       <main>
         <div>
-          <Container maxWidth="lg">
-            <Typography variant="h1" align="center" color="textPrimary" gutterBottom>
-             DONAPP
+          <Container maxWidth="lg" >
+          <Grid
+            item
+            xl={6}
+          >     
+    <ImageList xs={{ width: 620, height: 100 }} >
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} >
+          <img
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.title}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+    </Grid>
+    <Grid
+          container
+          spacing={5}
+        >
+          <Grid
+            item
+            lg={6}
+            md={12}
+            xl={6}
+            xs={12}
+          >
+             <Typography variant="h2" align="left" color="textPrimary" gutterBottom>
+             ¿Quienes somos?
             </Typography>
             <Typography variant="h4" align="left" color="textSecondary" paragraph>
             Somos una plataforma que realizamos, facilitamos y mejoramos la comunicación entre donadores de alimentos y comedores/ONG/fundaciones que necesiten ayuda.
@@ -37,6 +69,14 @@ const About = () => (
             <Typography variant="h4" align="left" color="textSecondary" paragraph>
             Le ofrecemos a comerciantes de grandes, medianos y pequeños comercios y/o supermercados la posibilidad de publicar aquellos productos próximos a vencerse que no hayan sido vendidos hasta el momento, para que, a través de la entrega o retiro de los mismos, puedan ayudar a personas que realmente lo necesiten, y puedan obtener también alguna reducción de sus impuestos. 
             </Typography>
+            </Grid>
+            <Grid
+            item
+            lg={6}
+            md={12}
+            xl={6}
+            xs={12}
+          >
             <Typography variant="h2" align="left" color="textPrimary" gutterBottom>
             Visión
             </Typography>
@@ -50,11 +90,37 @@ const About = () => (
             Esto se traduce en un compromiso por abordar el problema de las ingentes pérdidas de alimentos en el mundo, como clave para reducir el hambre, la pobreza, el impacto ambiental y los efectos del cambio climático.
             </Typography>
             <Typography variant="h2" align="left" color="textPrimary" gutterBottom>
-            Mail para alta de comedor/ONG/fundacion 
+            Comedor/ONG/fundacion 
             </Typography>
             <Typography variant="h4" align="left" color="textSecondary" paragraph>
-            contacto@donapp.com.ar
+            Si formas parte de una organización social que brinda asistencia alimentaria, podes contactarte con nosotros a través del mail:  
             </Typography>
+            <Typography variant="h4" align="left" color="#2619cf" paragraph>
+            contacto@donapp.com.
+            </Typography>
+            <Typography variant="h4" align="left" color="textSecondary" paragraph>
+            Podemos habilitarte una cuenta para que tengas acceso a este sitio web y puedas obtener donaciones de parte de distintos comercios. ¿Qué esperas para sumarte a DONNAP?
+            </Typography>
+            </Grid>
+            <Grid
+            item
+            lg={6}
+            md={12}
+            xl={6}
+            xs={12}
+          >
+            <AboutPhoto></AboutPhoto>
+            </Grid>
+            <Grid
+            item
+            lg={6}
+            md={12}
+            xl={6}
+            xs={12}
+          >
+            <AboutPhoto2></AboutPhoto2>
+            </Grid>
+            </Grid>
          </Container>
         </div>
       </main>
@@ -64,4 +130,10 @@ const About = () => (
  
 );
 
+const itemData = [
+{
+  img: 'https://res.cloudinary.com/dntepcqvn/image/upload/v1636932657/Logo_kok0fv.png',
+  title: 'Logo',
+},
+];
 export default About;
