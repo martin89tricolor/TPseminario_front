3e92c88010def3bb3d4431da652b49c7377b830d
martin89tricolor <66755493+martin89tricolor@users.noreply.github.com>
2021-11-14 21:26:06 -0300

    Update MainNavbar.js

--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,14 +1,25 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { AppBar, Toolbar } from '@material-ui/core';
+import { Avatar} from '@material-ui/core';
 
 const MainNavbar = (props) => (
   <AppBar
     elevation={0}
     {...props}
   >
-    <Toolbar sx={{ height: 64 }}>
+ <Toolbar sx={{ height: 80 }}>
      <RouterLink to="/">
-   DONAPP
+      <Avatar
+            src={'/static/images/Logo.png'}
+            alt="Product"
+            variant="square"
+            align= "left"
+            sx={{
+              height: 80,
+              width: 250,
+            }}
+          >
+          </Avatar>
       </RouterLink>
     </Toolbar>
   </AppBar>
